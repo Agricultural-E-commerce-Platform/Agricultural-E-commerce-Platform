632ccc2656cde8428b8caf6505c207f6915dc1bb
Jeong Jihun <doksakim7@naver.com>
2026-04-09 22:38:26 +0900

    chore: 자바와 스프링부트 프로젝트 기본 세팅, 필요한 의존성 주입(#7)

---
+++ src/test/java/com/spartafarmer/agri_commerce/AgriCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spartafarmer.agri_commerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgriCommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
