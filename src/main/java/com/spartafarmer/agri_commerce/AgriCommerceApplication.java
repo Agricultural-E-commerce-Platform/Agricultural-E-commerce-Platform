72b289ef0660d0c281dff5a45e88b7985fcc8bbe
Jihun Jeong <32298860+doksakim7@users.noreply.github.com>
2026-04-09 22:57:39 +0900

    [Chore] build.gradle - setting (#9)
    * chore: 자바와 스프링부트 프로젝트 기본 세팅, 필요한 의존성 주입(#7)

---
+++ src/main/java/com/spartafarmer/agri_commerce/AgriCommerceApplication.java
@@ -0,0 +1,13 @@
+package com.spartafarmer.agri_commerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgriCommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgriCommerceApplication.class, args);
+	}
+
+}
