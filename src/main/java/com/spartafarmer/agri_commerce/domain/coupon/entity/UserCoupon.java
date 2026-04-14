b8898680b99db70b7b2178c31dd4edcd5e25dd66
ljh <leejunghyun1019@gmail.com>
2026-04-14 14:06:37 +0900

    feat: 쿠폰 도메인 Entity/Repository 구현(#26)

--- src/main/java/com/spartafarmer/agri_commerce/domain/coupon/entity/UserCoupon.java
+++ src/main/java/com/spartafarmer/agri_commerce/domain/coupon/entity/UserCoupon.java
@@ -13,7 +13,7 @@ import java.time.LocalDateTime;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Table(name = "user_coupons")
+@Table(name = "useCoupons")
 public class UserCoupon extends BaseEntity {
 
     @Id
@@ -22,12 +22,12 @@ public class UserCoupon extends BaseEntity {
 
     // 쿠폰을 발급받은 회원
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id", nullable = false)
+    @JoinColumn(name = "userId", nullable = false)
     private User user;
 
     // 발급된 쿠폰
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "coupon_id", nullable = false)
+    @JoinColumn(name = "couponId", nullable = false)
     private Coupon coupon;
 
     @Enumerated(EnumType.STRING)
