ac9d6e5605888b30651b0734080fcc2d38b1219c
김예은 <rioloelux@gmail.com>
2026-04-20 20:33:12 +0900

    feat: 주문 재고차감 동시성 구현 (#140)

--- src/main/java/com/spartafarmer/agri_commerce/domain/cart/repository/CartRepository.java
+++ src/main/java/com/spartafarmer/agri_commerce/domain/cart/repository/CartRepository.java
@@ -3,10 +3,15 @@ package com.spartafarmer.agri_commerce.domain.cart.repository;
 import com.spartafarmer.agri_commerce.domain.cart.entity.Cart;
 import com.spartafarmer.agri_commerce.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface CartRepository extends JpaRepository<Cart, Long> {
 
     Optional<Cart> findByUser(User user);
+
+    @Query("SELECT c FROM Cart c JOIN FETCH c.cartItems ci JOIN FETCH ci.product WHERE c.user = :user")
+    Optional<Cart> findByUserWithItems(@Param("user") User user);
 }
